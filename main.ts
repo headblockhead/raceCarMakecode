dc4451925a96c7c06b1d6f816bb1c88690c341c2
headblockhead <26520767+headblockhead@users.noreply.github.com>
2021-10-30 20:27:51 +0100

    added light effects and smaller details

--- main.ts
+++ main.ts
@@ -2,32 +2,44 @@ namespace SpriteKind {
     export const animated = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    music.knock.play()
-    music.thump.play()
-    music.smallCrash.play()
-    music.zapped.play()
-    car.destroy(effects.fire, 100)
-    car_back.destroy(effects.fire, 200)
-    music.playMelody("E - C5 - G F E C ", 240)
+    if (started == 1) {
+        music.knock.play()
+        music.thump.play()
+        music.smallCrash.play()
+        music.zapped.play()
+        car.destroy(effects.fire, 100)
+        car_back.destroy(effects.fire, 200)
+        light.showAnimation(light.theaterChaseAnimation, 100)
+        light.setAll(0xff0000)
+        music.playMelody("E - C5 - G F E C ", 240)
+        game.splash("GAME OVER!", "Try again?")
+        game.reset()
+    }
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    music.jumpUp.play()
-    car.setVelocity(0, -75)
-    car.setImage(assets.image`car_red_up_0`)
-    pause(100)
-    car.setImage(assets.image`car_red_up_1`)
-    pause(200)
-    car.setVelocity(0, 75)
-    car.setImage(assets.image`car_red_up_0`)
-    pause(300)
-    car.setVelocity(0, 0)
-    car.setImage(assets.image`car_red`)
+    if (started == 1) {
+        effects.clearParticles(car_back)
+        music.jumpUp.play()
+        car.setVelocity(0, -75)
+        car.setImage(assets.image`car_red_up_0`)
+        pause(100)
+        car.setImage(assets.image`car_red_up_1`)
+        pause(200)
+        car.setVelocity(0, 75)
+        car.setImage(assets.image`car_red_up_0`)
+        pause(300)
+        car.setVelocity(0, 0)
+        car.setImage(assets.image`car_red`)
+        car_back.startEffect(effects.spray)
+    }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
 	
 })
 let car_back: Sprite = null
 let car: Sprite = null
+let started = 0
+started = 0
 music.playMelody("E - C5 - - G - F ", 240)
 light.showAnimation(light.cometAnimation, 1)
 light.setBrightness(20)
@@ -49,3 +61,4 @@ controller.moveSprite(car, 100, 0)
 car.setStayInScreen(true)
 controller.moveSprite(car_back, 100, 0)
 car_back.setStayInScreen(true)
+started = 1
